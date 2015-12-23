fd5cc23b0e9f94b335c39901e0ff8857fac861bb
Companion Cube <companion@craft.ai>
2015-12-23 10:41:26 +0100

    Publishing tutorials - 2015-12-23 09:41:26.541116

---
+++ static/actions.js
@@ -0,0 +1,207 @@
+var actionTable = [
+  'Say',
+  'GetFrontPage',
+  'ShowPic',
+  'GetCapitals',
+  'GetFirstElement',
+  'GetCityWeather',
+  'DisplayCityWeather',
+  'DisplayCityThumbnail',
+  'EndOfSub',
+  'Debug',
+  'Prompt'
+];
+
+function registerActions(onloadCB) {
+  var actionCopyTable = actionTable.slice(0);
+  function recursiveReg() {
+    var actionName = actionCopyTable.shift();
+    var actionObject = {'name':actionName, 'start':actionName, 'cancel':'cancel'};
+    registerAction(JSON.stringify(actionObject), actionCopyTable.length === 0 ? onloadCB : recursiveReg);
+  }
+  recursiveReg();
+}
+
+function Say(requestID, agentID, params) {
+  messages.innerHTML += '<br/>Agent ' + agentID + ' says: ' + params.message;
+  $('#results').animate({scrollTop: document.getElementById('messages').offsetHeight}, 1000);
+  sendSuccess(requestID);
+}
+
+function GetFrontPage(requestID, agentID, params) {
+  loading.innerHTML = 'loading...';
+  console.log('requesting front page');
+  $.getJSON('http://www.reddit.com/r/{0}/new/.json?limit=16&after=t3_{1}&show=all&sr_detail&jsonp=?'.format(params.subreddit, params.after), function(data) {
+    if (data.data.children.length === 0) {
+      sendFailure(requestID, JSON.stringify({'reason': 'No more posts here'}));
+    }
+    else {
+      var usefull = [];
+      var lastID = '';
+      $.each(data.data.children, function(index, child) {
+    var item = {};
+    item.domain = child.data.domain;
+    item.subreddit = child.data.subreddit;
+    item.url = child.data.url;
+    item.thumbnail = child.data.thumbnail;
+    item.name = child.data.title;
+    item.id = child.data.id;
+    item.permalink = child.data.permalink;
+    usefull.push(item);
+    lastID = item.id;
+  });
+      var out = {};
+      out.last = lastID;
+      out.frontpage = usefull;
+      sendSuccess(requestID, JSON.stringify(out));
+      loading.innerHTML = '';
+    }
+  });
+  this.addEventListener('error', function() {
+    sendFailure(requestID, JSON.stringify({'reason': 'Invalid subreddit name'}));
+  }, true);
+}
+
+function ShowPic(requestID, agentID, params) {
+  for (var i = 0; i < params.front.length; ++i) {
+    var kb = params.front[i];
+    loading.innerHTML = 'refreshing...';
+    if (kb.thumbnail.indexOf('http') === 0) {
+      messages.innerHTML += '<div class="col-md-3 col-sm-6""><div class="thumbnail" style="height:200px; overflow: hidden;"><a href="{0}" target="_blank"><img src="{1}" style="max-height: 100px;"></a><div class="caption"><p><a href="http://www.reddit.com/{3}" target="_blank">{2}</a></p></div></div></div>'.format(kb.url, kb.thumbnail, kb.name, kb.permalink, kb.subreddit);
+    }
+  }
+  $('#results').animate({scrollTop: document.getElementById('messages').offsetHeight}, 1000);
+  loading.innerHTML = '';
+  sendSuccess(requestID);
+}
+
+function GetCapitals(requestID, agentID, params) {
+  function matchCapital(name) {
+    var matching = '';
+    countries.some(function(i) {
+      var regex = new RegExp('\\b' + i.capital + '\\b', 'i');
+      regex.exec(name) !== null ? res = regex.toString().replace('/\\b', '').replace('\\b/i', '') : res = '';
+      matching = res;
+      return res;
+    });
+    return matching;
+  }
+  var cityList = [];
+  for (var i = 0; i < params.content.length; ++i) {
+    var kb = params.content[i];
+    var key = params.key || 'name';
+    var capitalName = matchCapital(kb[key]);
+    if (capitalName !== '') {
+      if (matchedCapitals.indexOf(capitalName) == -1) {
+        matchedCapitals.push(capitalName);
+        var j = 0;
+        for (var item in countries) {
+          if (countries[item].capital == capitalName) {
+            break;
+          }
+          j++;
+        }
+        var cityName = countries[j].name;
+        cityList.push({'name': cityName, 'pic': kb.thumbnail, 'url': kb.url});
+      }
+    }
+  }
+  sendSuccess(requestID, '{"capitals":' + JSON.stringify(cityList) + '}');
+}
+
+function GetFirstElement(requestID, agentID, params) {
+  if (params.array === null) {
+    sendFailure(requestID);
+  }
+  else {
+    var out = {};
+    var array = params.array;
+    out.element = array.shift();
+    out.capitals = array;
+    sendSuccess(requestID, '{"capitals":' + JSON.stringify(out.capitals) + ', "element":' + JSON.stringify(out.element) + '}');
+  }
+}
+
+function GetCityWeather(requestID, agentID, params) {
+  function getWeather(city) {
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + openWeatherAppId, function(data) {
+      if (data.weather !== undefined) {
+        sendSuccess(requestID, '{"weather":' + JSON.stringify(data.weather[0]) + '}');
+      }
+      else if (city.split(',').length == 3) {
+        city = city.split(',')[0] + ',' + city.split(',')[2];
+        getWeather(city);
+      }
+      else if (city.split(',').length == 2) {
+        city = city.split(',')[0];
+        getWeather(city);
+      }
+      else {
+        sendFailure(requestID, '{"weather": { "description": "No weather found for ' + city + '"}}');
+      }
+    })
+    .error(sendFailure(requestID));
+  }
+  getWeather(params.cityName);
+}
+
+function DisplayCityWeather(requestID, agentID, params) {
+  function displayOnMap(city, weather) {
+    geocoder.geocode({'address': city}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var img = {
+          url: 'http://openweathermap.org/img/w/' + weather.icon + '.png',
+          size: new google.maps.Size(50, 50),
+          anchor: new google.maps.Point(25, 25)
+        };
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          icon: img
+        });
+        map.panTo(results[0].geometry.location);
+        marker.setAnimation(google.maps.Animation.DROP);
+      }
+      else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+        sendFailure(requestID);
+      }
+    });
+  }
+  displayOnMap(params.cityName, params.cityWeather);
+  sendSuccess(requestID);
+}
+
+function DisplayCityThumbnail(requestID, agentID, params) {
+  capitalsID.innerHTML += '<div class="col-md-6"><div class="thumbnail capitalThumb" style="height: 160px; overflow: hidden;"><a href="{1}" target="_blank"><img src="{2}" style="max-height: 100px;"></a><div class="caption" style="text-align:center;"><strong>{0}</strong></div></div></div>'.format(params.city.name, params.city.url, params.city.pic);
+  $('#capitals').animate({scrollTop: document.getElementById('capitalsID').offsetHeight}, 1000);
+  sendSuccess(requestID);
+}
+
+function EndOfSub(requestID, agentID, params) {
+  $('#next').prop('disabled', true);
+  $('#nextFooter').prop('disabled', true);
+  messages.innerHTML += '<div class="col-md-3 col-sm-6"><div class="thumbnail endOfSub" style="height: 200px;"><img src="http://ipadwatcher.com/wp-content/uploads/2010/05/dead-end.jpg" style="height: 120px;"><div class="caption" style="text-align:center;"><h4>{0}</h4></div></div></div>'.format(params.message);
+  $('#results').animate({scrollTop: document.getElementById('messages').offsetHeight}, 1000);
+  loading.innerHTML = '';
+  sendSuccess(requestID);
+}
+
+function Debug(requestID, agentID, params) {
+  console.log('message from agent', agentID + ':', params.message);
+  sendSuccess(requestID);
+}
+
+function Prompt(requestID, agentID, params) {
+  var succeeds = function() {
+    $('#prompt').off('hidden.bs.modal', succeeds);
+    sendSuccess(requestID, '{"answer":' + JSON.stringify($('#promptinput').val()) + '}');
+  };
+  $('#prompt').on('hidden.bs.modal', succeeds);
+  $('#prompt').on('shown.bs.modal', function() {
+    $('#promptinput').focus();
+  });
+  $('#promptinput').prop('value', '');
+  promptmessage.innerHTML = params.message;
+  $('#prompt').modal('show');
+}
---
+++ static/runtime.js
@@ -0,0 +1,231 @@
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) { return typeof args[number] != 'undefined' ? args[number] : match; });
+};
+
+var instanceID;
+var agentID;
+console.log(window.location.href);
+var httpURL;
+var wsURL;
+var openWeatherAppId = '825c58f6363b3c1b01fe9532c746272c';
+
+function createInstance(user, project, version, onloadCB) {
+  httpURL = 'https://' + RUNTIME_URL + '/v1/' + user + '/' + project + '/' + version;
+  wsURL = 'wss://' + RUNTIME_URL + '/v1/' + user + '/' + project + '/' + version;
+  var oReq = new XMLHttpRequest();
+  oReq.open('PUT', httpURL + '?' + 'scope=app', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.onload = function() {
+    instanceID = JSON.parse(oReq.responseText).instance.instance_id;
+    onloadCB(instanceID);
+  };
+  oReq.onerror = function() {
+    alert('error while creating instance');
+  };
+  oReq.send();
+}
+
+function destroyInstance(onloadCB) {
+  console.log('delete');
+  //clearInterval( idUpdate );
+  var oReq = new XMLHttpRequest();
+  oReq.open('DELETE', httpURL + '/' + instanceID, true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.send();
+  oReq.onload = onloadCB;
+}
+
+function createAgent(behavior, knowledge, onloadCB, onErrorCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('PUT', httpURL + '/' + instanceID + '/agents', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.onload = function() {
+    onloadCB();
+  };
+  oReq.onerror = function() {
+    onErrorCB();
+  };
+  var params = {};
+  params.behavior = behavior;
+  params.knowledge = knowledge;
+  oReq.send(JSON.stringify(params));
+}
+
+function update() {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/update', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.onload = function() {
+  };
+  oReq.onerror = function() {
+    alert('error while updating the instance');
+  };
+  oReq.send('{"time":0.5,"ts":' + new Date().getTime() + '}');
+}
+
+function cancel(requestID, agentID, params) {
+  sendCancel(requestID);
+}
+
+function getAgentKnowledge(agentID, onloadCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('GET', httpURL + '/' + instanceID + '/agents/' + agentID + '/knowledge', true);
+  oReq.setRequestHeader('Content-type', 'application/json');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.onload = function() {
+    onloadCB(JSON.parse(oReq.responseText).knowledge);
+  };
+  oReq.onerror = function(e) {
+    alert('error ' + e.target.status + ' while retrieving the knowledge');
+  };
+
+  oReq.send();
+
+}
+
+function updateAgentKnowledge(agentID, destination, value, onloadCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/agents/' + agentID + '/knowledge', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+
+  oReq.onload = function() {
+    onloadCB();
+  };
+  var j = {};
+  j[destination] = value;
+  oReq.send(JSON.stringify(j));
+
+}
+
+function updateInstanceKnowledge(destination, value, onloadCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/instanceKnowledge', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+
+  oReq.onload = function() {
+    onloadCB();
+  };
+  var j = {};
+  j[destination] = value;
+  oReq.send(JSON.stringify(j));
+
+}
+
+function updateGlobalKnowledge(destination, value, onloadCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + simID + '/globalKnowledge', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+
+  oReq.onload = function() {
+    onloadCB();
+  };
+  var j = {};
+  j[destination] = value;
+  oReq.send(JSON.stringify(j));
+
+}
+
+function sendSuccess(requestID, jsonString) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/actions/' + requestID + '/success', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+
+  oReq.send(jsonString);
+}
+
+function sendFailure(requestID, jsonString) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/actions/' + requestID + '/failure', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.send(jsonString);
+}
+
+function sendCancel(requestID) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('POST', httpURL + '/' + instanceID + '/actions/' + requestID + '/cancelation', true);
+  oReq.setRequestHeader('content-type', 'application/html; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.send();
+}
+
+function registerAction(jsonString, onloadCB) {
+  var oReq = new XMLHttpRequest();
+  oReq.open('PUT', httpURL + '/' + instanceID + '/actions', true);
+  oReq.setRequestHeader('content-type', 'application/json; charset=utf-8');
+  oReq.setRequestHeader('accept', '');
+  oReq.setRequestHeader('X-Craft-Ai-App-Id', TUTO_APP_ID);
+  oReq.setRequestHeader('X-Craft-Ai-App-Secret', TUTO_APP_SECRET);
+  oReq.onload = function() {
+    console.log('action', JSON.parse(jsonString).name, 'registred');
+    onloadCB();
+  };
+  oReq.onerror = function() {
+    alert('error while registering action ' + JSON.parse(jsonString).name);
+  };
+  oReq.send(jsonString);
+}
+
+function doUpdate() {
+  idUpdate = setInterval(update, 500);
+}
+
+function doWS() {
+  var wsUrlRoute = wsURL + '/' + instanceID + '/websockets';
+  console.log('WS Connexion on', wsUrlRoute + ':');
+  if (wsUrlRoute) {
+    console.log('requesting WS connexion...');
+    ws = new WebSocket(wsUrlRoute);
+    ws.onmessage = function(evt) {
+      if (evt.data != 'ping') {
+        var jsonEvt = JSON.parse(evt.data);
+        console.log('WS data:', evt.data);
+        window[jsonEvt.call](jsonEvt.requestId , jsonEvt.agentID, jsonEvt.input);
+      }
+      else {
+        console.log('ping');
+      }
+      ws.send('Done');
+    };
+    ws.onopen = function() {
+      ws.send('socket open');
+      console.log('WS Connexion open', ws);
+    };
+    ws.onclose = function() {
+      console.log('WS Connexion closed', ws);
+    };
+    ws.onerror = function() {
+      console.log('WS Connexion error', ws);
+    };
+  }
+}
